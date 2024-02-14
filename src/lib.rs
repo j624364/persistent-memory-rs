496587dc6fc6f255c3874ca9f036654ff47a9431
Nameless <49117678+j624364@users.noreply.github.com>
2024-02-14 17:31:42 +0000

    Add retrieval and put into struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 mod storage_directory;
 
-use std::{fs, io, path::PathBuf};
+use std::{
+    fs,
+    io::{self, BufReader, BufWriter},
+    path::PathBuf,
+};
 pub use storage_directory::StorageDirectory;
 
 pub fn get_file_path(
@@ -16,14 +20,34 @@ pub fn get_file_path(
     path
 }
 
-pub fn store<T: serde::Serialize>(
-    storage_directory: StorageDirectory,
-    application_name: &str,
-    data_name: &str,
-    data: &T,
-) -> Result<(), io::Error> {
-    let file_path = get_file_path(storage_directory, application_name, data_name);
-    let serialised_json = serde_json::to_string(data).unwrap(); // todo: remove unwrap
+pub struct PersistentData {
+    file_path: PathBuf,
+}
 
-    fs::write(file_path, serialised_json)
+impl PersistentData {
+    pub fn new(
+        storage_directory: StorageDirectory,
+        application_name: &str,
+        data_name: &str,
+    ) -> Self {
+        Self {
+            file_path: get_file_path(storage_directory, application_name, data_name),
+        }
+    }
+
+    pub fn store<T: serde::Serialize>(&self, data: &T) -> Result<(), io::Error> {
+        let file = fs::File::open(&self.file_path)?;
+        let writer = BufWriter::new(file);
+        serde_json::to_writer_pretty(writer, &data).unwrap(); // todo: remove unwrap
+
+        Ok(())
+    }
+
+    pub fn retrieve<'a, T: for<'de> serde::Deserialize<'de>>(&self) -> Result<T, io::Error> {
+        let file = fs::File::open(&self.file_path)?;
+        let reader = BufReader::new(file);
+
+        let data = serde_json::from_reader(reader).unwrap(); // todo: remove unwrap
+        Ok(data)
+    }
 }
